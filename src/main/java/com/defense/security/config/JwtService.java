db9fcbe47a7a945db4509a8fb481cffbfb35b1e3
Atef Jlassi <Atef.jlassi@value.com.tn>
2023-01-18 14:45:14 +0100

    add javadoc to jwt service class

--- src/main/java/com/defense/security/config/JwtService.java
+++ src/main/java/com/defense/security/config/JwtService.java
@@ -13,23 +13,51 @@ import java.util.function.Function;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+/**
+ *  JwtService is a class that provides methods for creating and validating JSON web tokens.
+ *  @author Atef Jlassi
+ *  @version 1.0
+ */
 @Service
 public class JwtService {
 
   private static final String SECRET_KEY = "655368566D5971337436763979244226452948404D635166546A576E5A723475";
 
+  /**
+   *  Extracts the username from the given token.
+   *  @param token the token to extract the username from
+   *  @return the username contained in the token
+   */
   public String extractUsername(String token) {
     return extractClaim(token, Claims::getSubject);
   }
 
+  /**
+   * Extracts a claim from the given token using the specified claim resolver function.
+   * @param token the token to extract the claim from
+   * @param claimsResolver the function to use to resolve the claim
+   * @param <R> the type of the claim
+   * @return the extracted claim
+   */
   public <R> R extractClaim(String token, Function<Claims, R> claimsResolver) {
     final Claims claims = extractAllClaims(token);
     return claimsResolver.apply(claims);
   }
-
+  /**
+   *  Generates a token for the specified user details.
+   *  @param userDetails the user details to use to generate the token
+   *  @return the generated token
+   */
   public String generateToken(UserDetails userDetails) {
     return generateToken(new HashMap<>(), userDetails);
   }
+
+  /**
+   *  Generates a token for the specified user details and extra claims.
+   *  @param extraClaims the extra claims to include in the token
+   *  @param userDetails the user details to use to generate the token
+   *  @return the generated token
+   */
   public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
     return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
         .setIssuedAt(new Date(System.currentTimeMillis()))
@@ -38,24 +66,51 @@ public class JwtService {
         .compact();
   }
 
+  /**
+   *  Checks if the specified token is valid for the given user details.
+   *  @param token the token to check
+   *  @param userDetails the user details to use for validation
+   *  @return true if the token is valid, false otherwise
+   */
   public boolean isTokenValid(String token, UserDetails userDetails) {
     final String username = extractUsername(token);
     return (username.equals(userDetails.getUsername()) && !this.isTokenExpired(token));
   }
 
+  /**
+   *  Checks if the specified token is expired.
+   *  @param token the token to check
+   *  @return true if the token is expired, false otherwise
+   */
   private boolean isTokenExpired(String token) {
     return extractExpiration(token).before(new Date());
   }
 
+  /**
+   *   Extracts the expiration
+   *   date from the given token.
+   *  @param token the token to extract the expiration date from
+   *  @return the expiration date contained in the token
+   */
   private Date extractExpiration(String token) {
     return extractClaim(token, Claims::getExpiration);
   }
 
+  /**
+   * Extracts all claims from the given token.
+   * @param token the token to extract the claims from
+   * @return the claims contained in the token
+   */
   private Claims extractAllClaims(String token) {
     return Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(token)
         .getBody();
   }
 
+  /**
+   * Gets the signing key to be used for creating and validating tokens.
+   *
+   * @return the signing key
+   */
   private Key getSignInKey() {
     byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
     return Keys.hmacShaKeyFor(keyBytes);
--- src/main/java/com/defense/security/config/SecurityConfiguration.java
+++ src/main/java/com/defense/security/config/SecurityConfiguration.java
@@ -10,6 +10,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * SecurityConfiguration is a class that configures the security settings for the application.
+ *
+ * @author Atef Jlassi
+ * @version 1.0
+ */
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -17,20 +23,23 @@ public class SecurityConfiguration {
 
   private final JwtAuthenticationFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
+
+  /**
+   * Creates a bean for the security filter chain.
+   *
+   * @param httpSecurity HttpSecurity object that will be used to configure the security settings
+   * @return the security filter chain for the application
+   * @throws Exception
+   */
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity
-        .csrf()
+    httpSecurity.csrf()
         .disable()
-        .authorizeHttpRequests()
-        .requestMatchers("/api/v1/auth/**")
-        .permitAll()
-        .anyRequest()
-        .authenticated().and()
-        .sessionManagement()
+        .authorizeHttpRequests().requestMatchers("/api/v1/ /**").permitAll()
+        .anyRequest().authenticated().and().sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
         .authenticationProvider(authenticationProvider)
-        .addFilterBefore(jwtAuthFilter,UsernamePasswordAuthenticationFilter.class);
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
     return httpSecurity.build();
   }
